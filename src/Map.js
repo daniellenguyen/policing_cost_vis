2bf5344bfbec044e991b6a7017bf9e60e4993d81
daniellenguyen <daniellenguyen77@gmail.com>
2020-11-05 17:35:39 -0500

    cleaned and refactored, simulation doesn't do what I want but it at least moves the circles correctly

--- src/Map.js
+++ src/Map.js
@@ -1,60 +1,51 @@
 import React, { useEffect, useRef } from 'react'
 import * as d3 from 'd3'
 import './App.css'
-import * as allCoordinates from './data/us_cities.json'
-import * as renderCoordinates from './data/final_us_city_coordinates.json'
 import * as citiesData from './data/us_cities_with_FID.json'
-
 
 const Map = () => {
 
     const d3Container = useRef(null)
-
-    const findCityFID = (path) => {
-        const cities = allCoordinates.features
-
-        // Finding FID manually because findIndex uses strict equality
-        for (var i = 0; i <= cities.length; i++) {
-            const coordinates = cities[i]?.geometry?.coordinates
-            const pathCoordinates = path?.geometry?.coordinates
-            if (coordinates[0] === pathCoordinates[0] && coordinates[1] === pathCoordinates[1]) {
-                return i
-            }
-        }
-
-        // if coordinates aren't in there
-        return console.error("Coordinates not found")
-    }
-
-    const findCityUsingFID = (path) => {
-        const fid = findCityFID(path)
-        const cities = citiesData.default
-        return cities.find((city) => city.FID === fid)
-    }
 
     useEffect(() => {
         const svgContainer = d3.select(d3Container.current).append("svg")
             .attr("width", '100%')
             .attr("height", '100%')
             .attr("overflow", "visible")
-        
-        const projection = d3.geoAlbersUsa()
 
-        const geoGenerator = d3.geoPath()
-            .projection(projection)
-        
-        const u = svgContainer.selectAll('path').data(renderCoordinates.default)
-
-        u.enter()
-            .append('path')
-            .attr('d', geoGenerator)
+        const u = svgContainer.selectAll('circle')
+            .data(citiesData.default)
+            .enter()
+            .append('circle')
+            .attr("r", function(d) { return d.radius })
+            .attr("cx", function(d) { return d.cx })
+            .attr("cy", function(d) { return d.cy })
+            .style("stroke", "blue")
+            .style("fill", "white")
             .on("mouseover", function(d, path) {
-                d3.select(this).style("fill", "red")
-                console.log(findCityUsingFID(path)) // need this later for city name mouseover
+                d3.select(this)
+                .style("stroke", "red")
+                .style("fill", "white")
+                // console.log(path.City) // for city name mouseover
               })
             .on("mouseout", function(d) {
-                d3.select(this).style("fill", "black")
+                d3.select(this).style("stroke", "blue").style("fill", "none")
             })
+
+            const simulation = d3.forceSimulation(citiesData.default)
+              .velocityDecay(0.2)
+              .force("x", d3.forceX().strength(0.002))
+              .force("y", d3.forceY().strength(0.002))
+              .force("collide", d3.forceCollide().radius(d => d.r + 0.5).iterations(2))
+              .on("tick", ticked);
+
+        function ticked() {
+            u
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+        }
+            
+        simulation.on("tick", ticked)
 
     }, [])
     
--- src/data/clean_city_data.js
+++ src/data/clean_city_data.js
@@ -1,8 +1,9 @@
-// Get the coordinate objects in us_cities.json matching the cities in us_cities_with_FID.
-// Write these coordinate objects to a new json file.
 
+const d3 = require('d3')
 const fs = require('fs')
 const us_cities_with_FID = require('./us_cities_with_FID.json')
+const chosen_us_cities_coordinates = require('./final_us_city_coordinates.json')
+const all_us_cities_coordinates = require('./us_cities.json')
 
 const writeJsonToFile = (json, filename) => {
     const stringified_coordinates = JSON.stringify(json)
@@ -11,19 +12,54 @@ const writeJsonToFile = (json, filename) => {
     })
 }
 
+// Convert strings to numerical values. Was necessary for:
+// - Overall Policing Budget
+// - % city funds spent on policing
+// - City money per resident for police
+// const clean = (value) => {
+//     const cleanedBudget = value.replace(/\$|\,|\%/g, "")
+//     return parseInt(cleanedBudget)
+// }
 
+const budgetToRadius = d3.scaleLinear()
+    .domain([14493787, 11036298140]) // lowest to highest policing budget. 14 million to 1 billion
+    .range([5, 100]) //log scale alternative button. explain scaling decision in a small blurb. scaling decision gets across that ny spends 10 times more than any other city
+
+const coordinatesToFID = []
+const allCities = all_us_cities_coordinates.features
+chosen_us_cities_coordinates.map((point) => {
+    // Finding FID manually because findIndex uses strict equality
+    for (var i = 0; i < allCities.length; i++) {
+        const coordinates = allCities[i].geometry?.coordinates
+        const pathCoordinates = point.geometry?.coordinates
+        if (coordinates[0] === pathCoordinates[0] 
+            && coordinates[1] === pathCoordinates[1]) {
+            coordinatesToFID.push({
+                "FID": i,
+                "coordinates": pathCoordinates
+            })
+        }
+    }
+})
+
+// TODO write test on whether FID coordinates matches city name
+// TODO tests on proper type for policing budget etc
+// TODO data tests in general
 const cleaned_city_info = []
-const clean = (val) => {
-    const cleanedBudget = val.replace(/\$|\,|\%/g, "")
-    return parseInt(cleanedBudget)
-}
+const projection = d3.geoAlbers()
 
 us_cities_with_FID.map((city) => {
+    const radius = budgetToRadius(city.Overall_Policing_Budget)
+    const coordinates = coordinatesToFID.find((info) => info.FID === city.FID).coordinates
+    const projectedCoordinates = projection([coordinates[0], coordinates[1]])
     const newCity = {
-        ...city,
-        "Overall_Policing_Budget": clean(city.Overall_Policing_Budget),
-        "percent_city_funds_spent_on_policing": clean(city.percent_city_funds_spent_on_policing),
-        "city_money_per_resident_for_police": clean(city.city_money_per_resident_for_police),
+        ...city, // includes police dept. employee to resident ratio
+        "radius": radius,
+        "cx": projectedCoordinates[0],
+        "cy": projectedCoordinates[1],
+        "Overall_Policing_Budget": city.Overall_Policing_Budget,
+        "percent_city_funds_spent_on_policing": city.percent_city_funds_spent_on_policing,
+        "city_money_per_resident_for_police": city.city_money_per_resident_for_police,
 
     }
     cleaned_city_info.push(newCity)
