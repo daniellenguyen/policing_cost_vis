f08b746ebf04696e1b123df2d9d961beb2927191
daniellenguyen <daniellenguyen77@gmail.com>
2021-01-14 17:41:32 -0500

    Updates

---
+++ precache-manifest.003563ec77e537dbb1e36381a433e499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89d171aa15e24a19882637adff8f97c",
+    "url": "/policing_cost_vis/index.html"
+  },
+  {
+    "revision": "308eab42fbe9f685d34c",
+    "url": "/policing_cost_vis/static/css/main.dc3ef6fa.chunk.css"
+  },
+  {
+    "revision": "43966ea5d70caf4e72a2",
+    "url": "/policing_cost_vis/static/js/2.de5412fb.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/policing_cost_vis/static/js/2.de5412fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "308eab42fbe9f685d34c",
+    "url": "/policing_cost_vis/static/js/main.f309e968.chunk.js"
+  },
+  {
+    "revision": "d20dbad79397d95f55ca",
+    "url": "/policing_cost_vis/static/js/runtime-main.08481e94.js"
+  }
+]);
